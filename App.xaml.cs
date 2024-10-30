318bc82667ff79a321c251376a0f5bbaa5d752fa
cirillad <burba.2018london@gmail.com>
2024-10-30 15:30:12 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FamilyPlayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
