04d639a6aea84becc5dac82c7d92e38d3b8cc323
Assasin Creed <berezaoksana1@gmail.com>
2024-11-04 22:01:10 +0200

    create database

---
+++ Data/MusicPlayerContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FamilyPlayer.Models;
+
+namespace FamilyPlayer.Data
+{
+    public class MusicPlayerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<FavoriteSong> FavoriteSongs { get; set; }
+
+        public MusicPlayerContext(DbContextOptions<MusicPlayerContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Playlists)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.FavoriteSongs)
+                .WithOne(fs => fs.User)
+                .HasForeignKey(fs => fs.UserId);
+
+            modelBuilder.Entity<FavoriteSong>()
+                .HasKey(fs => new { fs.UserId, fs.SongId });
+            modelBuilder.Entity<Playlist>()
+                .HasMany(p => p.Songs)
+                .WithMany(s => s.Playlists)
+                .UsingEntity(j => j.ToTable("PlaylistSongs"));
+        }
+
+        public void SeedData()
+        {
+            if (!Users.Any())
+            {
+                Users.Add(new User
+                {
+                    Username = "testUser",
+                    Password = "hashedPassword",
+                    Playlists = new List<Playlist>
+                    {
+                        new Playlist { Name = "My First Playlist" }
+                    }
+                });
+                SaveChanges();
+            }
+        }
+    }
+}
